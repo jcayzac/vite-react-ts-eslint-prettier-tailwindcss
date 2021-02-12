3d863fb415465b437738281999f272fd137a15ea
Julien Cayzac <julien.cayzac@rakuten.com>
2021-02-12 15:44:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# README
+
+This template augments `pnpm init @vitejs/app app -- --template react-ts` with:
+
+- **only-allow pnpm** preinstall script.
+- **eslint** properly configured for rollup, along with **prettier**.
+- `~` and `~/public` aliases, mapping to `./src` and `./public` respectively.
+- **tailwindcss** and **autoprefixer**.
---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react'
+import logo from '~/logo.svg'
+
+function App(): JSX.Element {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-gray-900 py-12 px-4 sm:px-6 lg:px-8">
+      <header className="max-w-md w-full space-y-8 items-center">
+        <img src={logo} className="mx-auto h-12 w-auto" alt="logo" />
+        <p className="mt-6 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-100">
+          Hello Vite + React!
+        </p>
+        <p className="mt-2 text-center text-sm text-gray-600 dark:text-gray-400">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p className="text-center text-gray-900 dark:text-gray-100">
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p className="text-center">
+          <a
+            className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-pink-400"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="font-medium text-indigo-600 hover:text-indigo-500 dark:text-pink-400"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default App
